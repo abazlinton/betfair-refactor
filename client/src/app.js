f5a70fda4ff5608bcfd7ce316cbd2a082d3f4fde
Alex Bazlinton <abazlinton@gmail.com>
2017-01-09 02:00:03 +0000

    client displaying some returned data

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,27 @@
 window.onload = function() {
 
   var testButton = document.createElement('button');
-  testButton.innerText = "Test Button";
+  testButton.innerText = "Get Events";
+  testButton.onclick = getEvents();
   var body = document.getElementById('body');
   body.appendChild(testButton);
 
+}
 
+var getEvents = function(){
+  return function(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:3001/events");
+    request.onload = function(){
+    if (request.status === 200) {
+          var jsonString = request.responseText;
+          // var tableData = JSON.parse(jsonString);
+          var output = document.createElement('div');
+          output.innerText = jsonString;
+          body.appendChild(output);
+          };
+    };
+  console.log(request);
+  request.send();
+  }.bind(this);
 }
